1140874214592b921a503dc3bfadd7a584b2b173
slambeable <41022916+slambeable@users.noreply.github.com>
2022-03-26 22:10:19 +0300

    Initial Commit

---
+++ CustomIcon/CustomIconApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomIconApp.swift
+//  CustomIcon
+//
+//  Created by Андрей Евдокимов on 26.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomIconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
